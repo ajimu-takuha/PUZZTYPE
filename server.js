df45b15e3631233049a2e91ac16767d79b802c2d
AJIMU TAKUHA <ajimutakuha.idiot@gmail.com>
2024-12-24 18:44:04 +0900

    空メッセージ対処

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server);
 
-app.use(express.static(__dirname + '/public/'));
+app.use(express.static(__dirname + '/public'));
 
 const rooms = new Map();
 let waitingPlayer = null;
